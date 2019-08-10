39b634a38804beedcf454799608a1901ba712548
Matias Melendi <mmelendi@10pines.com>
2019-08-10 15:26:16 -0300

    muestra todos los postulados que ya fueron seleccionados, no sólo aquellos seleccionados por el usuario actual.

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -6,26 +6,20 @@ class ProfilesController < ApplicationController
     # Los usuarios que van a aparecer en el listado tienen que cumplir:
     #   1) Están postulados (tienen un perfil creado).
     #   2) No son el usuario actual (para evitar que una persona se seleccione a si misma).
-    #   3) No fueron invitados aún (No hay una invitación creada para ellos).
-    #   4) Todavía hay cupo para el rol que pusieron en su perfil.
-    #   5) Fueron seleccionados por el usuario actual para participar de la hackaton.
+    #   3) No fueron seleccionados aún y todavía hay cupo para el rol que pusieron en su perfil.
+    #   4) Fueron seleccionados para participar de la hackaton.
 
-    @my_invitation = current_user.invitation
     @postulados = User.
-        joins(:profile).
-        joins('LEFT JOIN invitations ON invitations.user_id = users.id').
-        where.not(profile_id: nil).
-        where.not(users: { id: current_user.id }).
-        where(
-          '(invitations.id IS NULL AND profiles.role IN (:available_roles)) OR users.id IN (:invited_by_me)',
-          invited_by_me: [
-            @my_invitation.invited_one_id,
-            @my_invitation.invited_two_id,
-          ],
-          available_roles: Role.with_available_invitations,
-        ).
-        order('organizer DESC, created_at ASC').
-        paginate(page: params[:page], per_page: 10)
+      joins(:profile).
+      joins('LEFT JOIN invitations ON invitations.user_id = users.id').
+      where.not(profile_id: nil).
+      where.not(users: { id: current_user.id }).
+      where(
+        '(invitations.id IS NULL AND profiles.role IN (:available_roles)) OR invitations.id IS NOT NULL',
+        available_roles: Role.with_available_invitations,
+      ).
+      order('organizer DESC, created_at ASC').
+      paginate(page: params[:page], per_page: 10)
   end
 
   def favorites
